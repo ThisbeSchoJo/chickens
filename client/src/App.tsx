93768f9c87096c323cb2bcddddde2b547f5148f2
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-09-10 20:15:55 -0700

    added typescript variable with type annotation to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 // The ": React.FC" part is TypeScript - aka this is a React Functional Component
 const App: React.FC = () => {
+  // This is a TypeScript variable with a type annotation!
+  const appName: string = "Chickens App";
+
   return (
     <div>
-      <h1>🐔 Chickens App</h1>
+      <h1>🐔 {appName}</h1>
       <p>Welcome to your TypeScript React app!</p>
     </div>
   );
