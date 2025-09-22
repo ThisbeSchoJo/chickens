67a808fd39e7e614469511c6bd2c2e6fceff12fa
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-09-21 22:17:33 -0700

    Added chicken to landing page

--- client/src/LandingPage.tsx
+++ client/src/LandingPage.tsx
@@ -9,20 +9,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onEnterApp }) => {
   return (
     <div className="landing-page">
       <div className="landing-content">
-        <div className="chicken-silhouette">
-          {/* White chicken silhouette using CSS */}
-          <div className="chicken-body"></div>
-          <div className="chicken-head"></div>
-          <div className="chicken-comb"></div>
-          <div className="chicken-beak"></div>
-          <div className="chicken-eye"></div>
-          <div className="chicken-wing"></div>
-          <div className="chicken-tail"></div>
-          <div className="chicken-legs">
-            <div className="leg left-leg"></div>
-            <div className="leg right-leg"></div>
-          </div>
-        </div>
+        <div className="chicken-silhouette">🐓</div>
 
         <div className="landing-text">
           <h1>Chickens</h1>
